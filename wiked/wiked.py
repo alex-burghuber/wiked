71b49b11e0c3af1f54332c51b122190f49fc079b
Alex <alexander.burghuber@gmail.com>
2023-12-13 15:38:53 +0100

    Added count, timestamp and nightowls command

--- wiked/wiked.py
+++ wiked/wiked.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import requests
+import random
 
 import caching
 import word_clouds
@@ -64,9 +65,9 @@ def query_contributions(given_params: {}, language_code: str) -> list:
 def command_loop(ip_range: str, contributions: list, is_jku: bool):
     while True:
         print("\n")
-        print("Enter a command (wc, exit)")
+        print("Enter a command (wc, timestamp, nightowls, exit)")
 
-        command = input().lower()
+        command = input().strip().lower()
         if command == "wc":
             titles = [contribution["title"].replace(" ", "_") for contribution in contributions]
             text = " ".join(titles)
@@ -76,6 +77,23 @@ def command_loop(ip_range: str, contributions: list, is_jku: bool):
             else:
                 name = ip_range.replace("/", "_") + ".png"
                 word_clouds.make_word_cloud(text, name)
+
+        elif command == "count":
+            print("Number of contributions: %s" % len(contributions))
+        elif command == "timestamp":
+            print("Show all contributions after a timestamp")
+            print("Enter a timestamp (e.g. 2023-01-01T00:00:00Z)")
+            timestamp = input()
+            after_timestamp = [c for c in contributions if c["timestamp"] > timestamp]
+            print("Contributions after %s: %s" % (timestamp, len(after_timestamp)))
+            for c in after_timestamp:
+                print("%s at %s" % (c["title"], c["timestamp"]))
+        elif command == "nightowls":
+            print("25 random night owls:")
+            night_owls = [c for c in contributions if 22 <= int(c["timestamp"][11:13]) or int(c["timestamp"][11:13]) < 6]
+            random.shuffle(night_owls)
+            for owl in night_owls[:25]:
+                print("Edited %s at %s" % (owl["title"], owl["timestamp"]))
         elif command == "exit":
             break
         else:
